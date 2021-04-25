42d7cb324fe28593ee38e84161dea3c8e10f53d3
Matthew Goad <matthewxgoad@gmail.com>
2021-04-25 18:24:45 -0400

    starting creating database connections and schema

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 
 const connection = mysql.createConnection({
-    host: process.env.HOST,
-    port: process.env.PORT || 1154,
-    user: process.env.USER,
-    password: process.env.PASS,
-})
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || '',
+    user: process.env.DB_USER || '',
+    password: process.env.DB_PASS,
+});
+
+connection.connect((err) => {
+    if(err) throw err;
+    console.log("Connected at " + connection.threadId + "\n");
+    connection.end()
+});
+
